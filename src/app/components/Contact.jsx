5e95d11f30560b531e2ac657241378747f9b6f69
Eduardo Miyake <dudsmiyake@gmail.com>
2024-08-11 21:31:06 -0300

    footer update

--- src/app/components/Contact.jsx
+++ src/app/components/Contact.jsx
@@ -3,9 +3,9 @@ import Link from "next/link";
 
 const Contact = () => {
   return (
-    <div className="flex bg-stone-950 w-full h-48 text-white p-4 justify-between" id="Contact">
-      <div className="flex flex-col mt-4 w-fit">
-        <h1 className="">My contacts:</h1>
+    <div className="flex bg-stone-950 w-full h-fit text-white p-4 justify-between" id="Contact">
+      <div className="flex flex-col mt-4 w-fit items-center md:items-start">
+        <h1 className="w-fit">My contacts:</h1>
         <div className="flex w-fit py-4 justify-around">
           <div className="p-1 h-fit hover:-translate-y-1 transition ease-in">
             <Link href={"https://www.linkedin.com/in/eduardo-miyake/"}>
@@ -17,7 +17,7 @@ const Contact = () => {
               <Image alt="github icon" src={"/icons/github.svg"} width={30} height={30} />
             </Link>
           </div>
-          <div className="flex p-1 h-fit text-sm text-gray-400">
+          <div className="hidden md:flex p-1 h-fit text-sm text-gray-400">
             <Image alt="gmail icon" src={"/icons/gmail.svg"} width={30} height={30} />
             <p className="ml-1 place-self-center">@</p>
             <p className="text-sm place-self-center select-all">
@@ -25,7 +25,11 @@ const Contact = () => {
             </p>
           </div>
         </div>
-        <div className="md:hidden mx-8 text-sm opacity-75">Developed using Next.js, React.js and deployed with Vercel</div>
+        <div className="text-sm mb-12 md:hidden">
+          <span>@</span>
+          <span>eduardomiyake.martins@gmail.com</span>
+        </div>
+        <div className="md:hidden mx-8 text-sm opacity-50">Developed using Next.js, React.js and deployed with Vercel</div>
       </div>
       <div className="hidden md:flex items-center opacity-75">
         <h1>Developed using Next.js, React.js and deployed with Vercel</h1>
